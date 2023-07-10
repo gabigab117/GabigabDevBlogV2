a2e5752ed9fad2a01dc75f8c6d376ae83bc81e52
gabigab117 <gabrieltrouve5@outlook.com>
2023-07-10 23:47:05 +0200

    env2

--- PyGabdevBlog/PyGabdevBlog/settings/base.py
+++ PyGabdevBlog/PyGabdevBlog/settings/base.py
@@ -18,7 +18,7 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 env = environ.Env()
-environ.Env.read_env(env_file=BASE_DIR / ".env")
+environ.Env.read_env(env_file=str(BASE_DIR / ".env"))
 
 
 # Quick-start development settings - unsuitable for production
